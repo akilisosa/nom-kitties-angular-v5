0152b9ab0b339f4d8f5f0e4f6a373c1a50a24f19
akili sosa <akilisosa@gmail.com>
2024-11-22 00:33:48 -0600

    all augh up

---
+++ src/app/game-hub/room/components/controller/controller.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-controller',
+  imports: [],
+  templateUrl: './controller.component.html',
+  styleUrl: './controller.component.css'
+})
+export class ControllerComponent {
+
+}
